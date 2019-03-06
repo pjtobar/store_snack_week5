90a2fbf8776b4faeca67576c83d0f1ee98346745
pjtobar <pablo.tobar711@gmail.com>
2019-03-06 14:32:18 -0600

    init commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  def admin?
+    has_role?(:admin)
+  end
+
+  def client?
+    has_role?(:client)
+  end 
+
+end
