f9172b8883ea0de0886e2f78a2a7637a2361c698
pjtobar <pablo.tobar711@gmail.com>
2019-03-19 14:58:43 -0600

    refactoring models details and product with delegate

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -2,15 +2,5 @@ class Detail < ApplicationRecord
   belongs_to :product
   belongs_to :purchase
 
-  def product_name
-    product.name
-  end
-
-  def product_sku
-    product.sku
-  end
-
-  def product_price
-    product.price
-  end
+  delegate :name, :sku, :price, :category_name, to: :product, prefix: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,19 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :details
-  has_many :purchases, :through => :details
-  has_and_belongs_to_many :tags
+  has_many :purchases, through: :details
   has_many :likes
-  has_many :users, :through => :likes
+  has_many :users, through: :likes
+  has_and_belongs_to_many :tags
   has_one_attached :image
   validates :sku, uniqueness: true
 
   scope :available, -> { where(state: 1) }
 
+  delegate :name, to: :category, prefix: true
+
+
   def product_image
     return self.image.variant(resize: '220x220').processed
-  end
-
-  def category_name
-    category.name
   end
 end
